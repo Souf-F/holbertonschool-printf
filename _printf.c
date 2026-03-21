36a6aeb4d6b3c981d4f4f26478ddbabd9993a1ee
Souf-F <sfecom.31000@gmail.com>
2026-03-21 23:00:36 +0100

    "feat: add base structure of _printf with va_list and format loop"

---
+++ _printf.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _printf - produces output according to a format
+ * @format: character string with directives
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+
+	va_list args;
+	int count;
+	int index;
+
+	index = 0;
+	count = 0;
+
+	va_start(args,  format);
+
+	while (format[index] != '\0')
+	{
+		index++;
+	}
+
+	return (0);
+}
